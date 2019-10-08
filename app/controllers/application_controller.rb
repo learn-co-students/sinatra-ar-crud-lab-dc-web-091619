6b92716efec66ef7b3fa8188794471813bd6333a
github username <github email address>
2019-10-08 14:16:54 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 
-require_relative '../../config/environment'
+require_relative '../../config/environment' 
 
 class ApplicationController < Sinatra::Base
 
@@ -8,6 +8,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @magazines = Article.all 
+
+    erb :index
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    @title = params[:title]
+    @content = params[:content]
+    @new_article = Article.create(title: @title, 
+    content: @content)
+    
+    redirect "/articles/#{@new_article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit 
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title],
+    content: params[:content])
+    @article.save
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect '/articles'
+  end
+
 end
